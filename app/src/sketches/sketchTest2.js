0996a01fa1743148a90a76c3554e921039a6ac66
Long <toivabanlaban@gmail.com>
2018-11-15 22:18:29 +0700

    timer

--- app/src/sketches/sketchTest2.js
+++ app/src/sketches/sketchTest2.js
@@ -7,14 +7,4 @@ export default function sketchTest2(p) {
     p.createCanvas(400, 600);
     p.background("#ddd");
   };
-  p.draw = () => {
-    p.strokeWeight(8);
-    p.stroke(0);
-    if (p.mouseIsPressed) {
-      p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
-    }
-  };
-  p.mousePressed = () => {
-    console.log(ai.predict());
-  };
 }
